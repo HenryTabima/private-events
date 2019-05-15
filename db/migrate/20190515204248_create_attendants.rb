ec586d6432bc0941a2adf876d9d037c2fa1312bc
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-15 17:29:41 -0500

    [fix] foreing key errors

--- db/migrate/20190515204248_create_attendants.rb
+++ db/migrate/20190515204248_create_attendants.rb
@@ -1,10 +1,12 @@
 class CreateAttendants < ActiveRecord::Migration[5.2]
   def change
-    create_table :attendants do |t|
-      t.references :attendee, foreign_key: true
-      t.references :attended_event, foreign_key: true
+    create_table :attendants, index: false do |t|
+      t.references :attendee, index: true
+      t.references :attended_event, index: true
 
       t.timestamps
     end
+    add_foreign_key :attendants, :users, column: :attendee_id
+    add_foreign_key :attendants, :events, column: :attended_event_id
   end
 end
