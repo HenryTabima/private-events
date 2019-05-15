9420dc94aa6f42d20fb49cdbc4ebb59651332519
Sunny <ring2rings93@gmail.com>
2019-05-15 19:19:04 +0100

    events, relationships

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,5 +12,6 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    logout
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!@current_user
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
