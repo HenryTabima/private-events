763912057f84c7e076fef9591b43a0d24be7976b
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-17 20:27:22 -0500

    [update] users show view and model scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: "User"
+  belongs_to :creator, class_name: 'User'
   has_many :attendants, foreign_key: :attended_event_id
   has_many :attendees, through: :attendants, source: :attendee
 
   scope :past, -> { where('event_date < ?', DateTime.current) }
 
-  def Event.upcoming
-    self.where('event_date > ?', DateTime.current)
+  def self.upcoming
+    where('event_date > ?', DateTime.current)
   end
-
-
 end
